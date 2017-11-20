6a4f4ec5752966a2ab14c3a31dde3584b047c197
Ankai <ankaijie@gmail.com>
2017-11-19 17:56:40 -0800

    added simple login

--- app.py
+++ app.py
@@ -1,15 +1,40 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, session, redirect, g, url_for
 from celery import Celery
 from config import APP_NAME
 from tasks import run_overcite_script, store_request
 from datapython.apilib import ScopusApiLib
+from datapython.credentials import LOGINUSER, LOGINPASS
 import pprint
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(20)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        session.pop('user', None)
+        if request.form['username'].lower() == LOGINUSER and request.form['password'] == LOGINPASS:
+            session['user'] = request.form['username']
+            return redirect('/index')
+        else:
+            return render_template('login.html', passStatus='Incorrect Login')
+    return render_template('login.html')
+
+
+@app.route('/index')
 def index():
-    return render_template('index.html')
+    if g.user:
+        return render_template('index.html')
+
+    return redirect(url_for('login'))
+
+
+@app.before_request
+def before_request():
+    g.user= None
+    if 'user' in session:
+        g.user = session['user']
 
 
 @app.route('/request/overcitesAuthor', methods=['POST'])
